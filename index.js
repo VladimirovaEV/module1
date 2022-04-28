a2e1182bd082a1ee8af03d5c66c612730a8468fd
Vladimirova Ekaterina <echapyrkina@mail.ru>
2022-04-28 18:09:29 +0300

    добавлен товар

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const productName = "яблоко";
-let quantityOfProduct = 2;
-const categoryOfProduct = "фрукт";
-const priceOfProduct = 100;
+{
+    const productName = "яблоко";
+    const quantityOfProduct = 2;
+    const categoryOfProduct = "фрукты";
+    const priceOfProduct = 100;
 
-const sum = quantityOfProduct * priceOfProduct;
+    const sum = quantityOfProduct * priceOfProduct;
 
-console.log(sum);
+    console.log(productName);
+    console.log(sum);
+}
+
+{
+    const newProductName = "картофель";
+    const quantityOfNewProduct = 5;
+    const categoryOfNewProduct = "овощи";
+    const priceOfNewProduct = 70;
+}

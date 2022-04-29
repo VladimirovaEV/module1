4b4d08d8dd55ac69a89339f54edb4c957e50639f
Vladimirova Ekaterina <echapyrkina@mail.ru>
2022-04-29 18:36:52 +0300

    добавлена папка lesson3, скопированы в нее файлы

---
+++ lesson3/index.js
@@ -0,0 +1,18 @@
+{
+    const productName = "яблоко";
+    const quantityOfProduct = 2;
+    const categoryOfProduct = "фрукты";
+    const priceOfProduct = 100;
+
+    const sum = quantityOfProduct * priceOfProduct;
+
+    console.log(productName);
+    console.log(sum);
+}
+
+{
+    const newProductName = "картофель";
+    const quantityOfNewProduct = 5;
+    const categoryOfNewProduct = "овощи";
+    const priceOfNewProduct = 70;
+}

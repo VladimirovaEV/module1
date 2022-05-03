c6a79bfb6909c51eb9213f39a1fbeb0b8f6dca05
Vladimirova Ekaterina <echapyrkina@mail.ru>
2022-05-03 22:32:24 +0300

    lesson 6

---
+++ lesson 6/Task02.js
@@ -0,0 +1,10 @@
+const isPrime = (num) => {
+    for (i = 2; i < num; i++ ) {
+        if (num % i == 0) {
+            return false;
+        }
+    } 
+    return true;
+}
+
+console.log(isPrime(5))

c089421c83505b0a3519d09443ce18be8eb8905a
Vladimirova Ekaterina <echapyrkina@mail.ru>
2022-05-22 15:55:34 +0300

    game on english

--- lesson 13/game.js
+++ lesson 13/game.js
@@ -1,51 +1,79 @@
 'use strict';
 (() => {
   const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
+  const FIGURES_ENG = ['rock', 'scissors', 'paper'];
   const getRandomIntInclusive = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
+  let language = confirm('ENG') ? 'ENG' : '';
+
   const game = (language) => {
     const result = {
       player: 0,
       computer: 0,
     };
+
+    const lang = language === 'EN' || language === 'ENG' ?
+      FIGURES_ENG : FIGURES_RUS;
+
     return function start() {
-      const computerFigure = (FIGURES_RUS[getRandomIntInclusive(0, 2)]);
+      const computerFigure = (lang[getRandomIntInclusive(0, 2)]);
       console.log(computerFigure);
-      const playerFigure = ((prompt('камень, ножницы, бумага?')).toLowerCase()).trim();
-      const outMessage = function() {
-        confirm('Точно ли вы хотите выйти?')
+      const playerFigure = ((prompt(lang)).toLowerCase()).trim();
+      const outMessage = function(language) {
+        if (language === 'ENG') {
+          confirm('Are you sure you want to get out?')
+        ? alert(`Result:\nComputer: ${result.computer}\nPlayer: ${result.player}`) : start();
+        return;
+        } else {
+          confirm('Точно ли вы хотите выйти?')
         ? alert(`Результат:\nКомпьютер: ${result.computer}\nИгрок: ${result.player}`) : start();
         return;
+        };
       };
-      const message = function() {
-          confirm('Ещё?') ? start() : outMessage();
+      const message = function(language) {
+        if (language === 'ENG') {
+          confirm('Continue?') ? start() : outMessage(language);
+        } else {
+          confirm('Ещё?') ? start() : outMessage(language);
+        }
       };
-      let playerFigureResult = FIGURES_RUS.find(item => item.charAt(0) === playerFigure.charAt(0));
+      let playerFigureResult = lang.find(item => item.charAt(0) === playerFigure.charAt(0));
       if (playerFigure === '') {
         start();
       };
       if (playerFigure === computerFigure) {
-        alert(`Компьютер: ${computerFigure}\nВы: ${playerFigure}\nНичья`);
+        if (language === 'ENG') {
+           alert(`Computer: ${computerFigure}\nYou: ${playerFigure}\nDraw`);
+        } else {
+           alert(`Компьютер: ${computerFigure}\nВы: ${playerFigure}\nНичья`);
+        }
         start();
       }
-      if ((computerFigure === FIGURES_RUS[0] && playerFigure.charAt(0) === FIGURES_RUS[2].charAt(0)) || (computerFigure === FIGURES_RUS[1] && playerFigure.charAt(0) === FIGURES_RUS[0].charAt(0)) || (computerFigure === FIGURES_RUS[2] && playerFigure.charAt(0) === FIGURES_RUS[1].charAt(0))) {
+      if ((computerFigure === lang[0] && playerFigure.charAt(0) === lang[2].charAt(0)) || (computerFigure === FIGURES_RUS[1] && playerFigure.charAt(0) === lang[0].charAt(0)) || (computerFigure === lang[2] && playerFigure.charAt(0) === lang[1].charAt(0))) {
         result.player += 1;
-        alert(`Компьютер: ${computerFigure}\nВы: ${playerFigureResult}\nВы выиграли`);
-        message();
+        if (language === 'ENG') {
+          alert(`Computer: ${computerFigure}\nYou: ${playerFigureResult}\nYou win`);
+        } else {
+          alert(`Компьютер: ${computerFigure}\nВы: ${playerFigureResult}\nВы выиграли`);
+        }
+        message(language);
       } else {
         result.computer += 1;
-        alert(`Компьютер: ${computerFigure}\nВы: ${playerFigureResult}\nКомпьютер выиграл`);
-        message();
+        if (language === 'ENG') {
+          alert(`Computer: ${computerFigure}\nYou: ${playerFigureResult}\nComputer win`);
+        } else {
+          alert(`Компьютер: ${computerFigure}\nВы: ${playerFigureResult}\nКомпьютер выиграл`);
+        }
+        message(language);
       }
-
       if (playerFigure === null) {
-        outMessage();
+        outMessage(language);
       }
     };
   };
-  window.RPS = game;
+  window.RPS = game(language);
 })();
 
